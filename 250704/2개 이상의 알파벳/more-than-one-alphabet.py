42d0055eac3687ddf4d4c582749fb17ab58cda23
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-04 17:25:11 +0900

    Title: 2개 이상의 알파벳, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250704/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 250704/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,7 +1,10 @@
 A = input()
 n = 0
 # Please write your code here.
-for i in range(len(A) - 1):
-    n = n + 1 if A[i] != A[i + 1] else n
+def func(s: str) -> bool:
+    for i in range(len(s)):
+        if s[i] != s[0]:
+            return True
+    return False
 
-print("Yes" if n >= 2 else "No")
+print("Yes" if func(A) else "No")
