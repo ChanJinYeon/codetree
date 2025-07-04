a42472ac1df5182bca5dd0bc580e498f2413a800
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-04 16:26:54 +0900

    Title: 최소공배수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250704/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,8 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def gcd(a: int, b: int) -> int:
+    return b if a % b == 0 else gcd(b, a % b)
+
+g = gcd(n, m)
+print(int(n * m / g))
