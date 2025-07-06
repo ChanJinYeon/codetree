c7039d2b53080f4c39d1b3aa79333e7bfa309efb
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-06 23:30:49 +0900

    Title: 이상한 수열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250706/이상한 수열/a-strange-sequence.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+# Please write your code here.
+def func(N: int) -> int:
+    if N == 1:
+        return 1
+    if N == 2:
+        return 2
+    return func(N // 3) + func(N - 1)
+
+print(func(N))
