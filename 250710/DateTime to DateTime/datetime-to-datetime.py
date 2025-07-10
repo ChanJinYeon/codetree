42450b8c4b44810691f43dc196476563499e2185
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-10 23:11:13 +0900

    Title: DateTime to DateTime, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/DateTime to DateTime/datetime-to-datetime.py
+++ 250710/DateTime to DateTime/datetime-to-datetime.py
@@ -2,4 +2,6 @@ a, b, c = map(int, input().split())
 day_to_minute = 24 * 60
 hour_to_minute = 60
 
-print((a-11)*day_to_minute + (b-11)*hour_to_minute + c-11)
+ans = (a-11)*day_to_minute + (b-11)*hour_to_minute + c-11
+
+print(ans) if ans >= 0 else print(-1)
