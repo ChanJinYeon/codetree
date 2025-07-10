faa2334751b3495f144d04f1840a1f69696056c6
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-10 23:29:22 +0900

    Title: 요일 맞추기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/요일 맞추기/guess-day-of-week.py
+++ 250710/요일 맞추기/guess-day-of-week.py
@@ -5,8 +5,8 @@ sum_month = [0]
 for i in range(len(month)-1):
     sum_month.append(sum_month[i] + month[i + 1])
 
-# Please write your code here.
-calc = month[m2-1] + d2 - month[m1-1] - d1 + 1
-calc_date = (calc + 7) % 7
+calc = sum_month[m2-1] + d2 - sum_month[m1-1] - d1
+# 시작 요일이 "Mon"이므로 + 1
+calc_date = ((calc  + 1) + 7) % 7
 
 print(date[calc_date])
