2df19dc0bf0af057a87210c04f64f2b5ffdff6ce
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-10 23:05:32 +0900

    Title: Date to Date, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/Date to Date/date-to-date.py
+++ 250710/Date to Date/date-to-date.py
@@ -1,4 +1,8 @@
 m1, d1, m2, d2 = map(int, input().split())
-num_of_days = [0, 3, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+sum_days = [0]
 # Please write your code here.
-print(sum(num_of_days[m1+1:m2+1])+d2-d1)
+for i in range(len(num_of_days) - 1):
+    sum_days.append(sum_days[i] + num_of_days[i + 1])
+
+print(sum_days[m2-1] + d2 - sum_days[m1-1] - d1 + 1)
