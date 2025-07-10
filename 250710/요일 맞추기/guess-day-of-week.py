c8d86f52b14f3160ea7198094b26d5d5c5cd34ad
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-10 23:34:46 +0900

    Title: 요일 맞추기, Time: 57ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/요일 맞추기/guess-day-of-week.py
+++ 250710/요일 맞추기/guess-day-of-week.py
@@ -1,12 +1,11 @@
 m1, d1, m2, d2 = map(int, input().split())
 date = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-sum_month = [0]
-for i in range(len(month)-1):
-    sum_month.append(sum_month[i] + month[i + 1])
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-calc = sum_month[m2-1] + d2 - sum_month[m1-1] - d1
-# 시작 요일이 "Mon"이므로 + 1
-calc_date = ((calc  + 1) + 7) % 7
+def calc(m: int, d: int) -> int:
+    return sum(month[:m-1]) + d
 
-print(date[calc_date])
+calc_days = calc(m2, d2) - calc(m1, d1)
+start_day = 1 # Mon
+
+print(date[(calc_days + start_day + 7) % 7])
