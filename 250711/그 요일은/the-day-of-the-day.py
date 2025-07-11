6c4aea291cbe9939d1c4292b2098d18dbc9903cf
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-11 17:13:21 +0900

    Title: 그 요일은, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250711/그 요일은/the-day-of-the-day.py
+++ 250711/그 요일은/the-day-of-the-day.py
@@ -3,16 +3,14 @@ A = input()
 
 month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 date = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-start_date = 0
+start_date = date.index("Mon")
+target_date = date.index(A)
+sub_date = target_date - start_date
 
 def calc(m: int, d: int) -> int:
     return sum(month[:m-1]) + d
 
 calc_day = calc(m2, d2) - calc(m1, d1) + 1
 
-for i in range(len(date)):
-    if date[i] == A:
-        start_date = i
-
-ans = (calc_day - start_date) // 7 + 1
+ans = (calc_day - sub_date -1) // 7 + 1
 print(ans)
