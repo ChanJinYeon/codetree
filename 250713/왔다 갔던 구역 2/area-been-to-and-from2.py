230bded4a3511a215a68b2bf43fb42e0dc01ca6a
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-13 12:36:16 +0900

    Title: 왔다 갔던 구역 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250713/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 250713/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -8,8 +8,10 @@ lst = [0 for _ in range(2001)]
 for a, b in segments:
     if b == 'L':
         lst[idx-int(a)+1:idx+1] = [x+1 for x in lst[idx-int(a):idx]]
+        idx = idx - int(a) + 1
     if b == 'R':
         lst[idx:idx+int(a)] = [x+1 for x in lst[idx:idx+int(a)]]
+        idx = idx + int(a)
     
 for i in lst:
     if i >= 2:
