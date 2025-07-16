b1870b14f9cd25757b576ff09c2768b13dcf1199
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-16 21:27:37 +0900

    Title: 좌우로 움직이는 로봇, Time: 332ms, Memory: 38MB, Status: Passed - Codetree

--- 250716/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250716/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,31 +1,39 @@
-from typing import List, Tuple
+from typing import List, Tuple, Iterator
 
-def func(move: List[Tuple[int, int]]) -> List[int]:
-    lst = [0]
+def pos(move: List[Tuple[int, int]]) -> Iterator[int]:
+    pos = 0
     for m_str, d in move:
         m = int(m_str)
         step = 1 if d == 'R' else -1
         for _ in range(m):
-            lst.append(lst[-1] + step)
-    return lst
-
+            pos += step
+            yield pos        
+    
 n, m = map(int, input().split())
 n_move = [tuple(input().split()) for _ in range(n)]
 m_move = [tuple(input().split()) for _ in range(m)]
 
-n_lst = func(n_move)
-m_lst = func(m_move)
-max_len = max(len(n_lst), len(m_lst))
-n_lst += [n_lst[-1]] * (max_len - len(n_lst))
-m_lst += [m_lst[-1]] * (max_len - len(m_lst))
-
-dev_lst = [n_lst[i] - m_lst[i] for i in range(max_len)]
+n_gen = pos(n_move)
+m_gen = pos(m_move)
 
 ans = 0
-last_sign = 0
-    
-for i in range(1, max_len):
-    if n_lst[i] == m_lst[i] and n_lst[i-1] != m_lst[i-1]:
+pos_n = pos_m = 0
+last_equal = True
+done_n = done_m = False
+
+while not(done_n and done_m):
+    if not done_n:
+        try:
+            pos_n = next(n_gen)
+        except StopIteration:
+            done_n = True
+    if not done_m:
+        try:
+            pos_m = next(m_gen)
+        except StopIteration:
+            done_m = True
+    if not last_equal and pos_n == pos_m:
         ans += 1
+    last_equal = (pos_n == pos_m)
 
 print(ans)
