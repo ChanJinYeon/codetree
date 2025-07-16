cfb6f254e81eb2e3f7011e094773a3f2e517f86e
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-16 21:07:10 +0900

    Title: 좌우로 움직이는 로봇, Time: 572ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250716/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250716/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -15,30 +15,17 @@ m_move = [tuple(input().split()) for _ in range(m)]
 
 n_lst = func(n_move)
 m_lst = func(m_move)
-len_n_lst = len(n_lst)
-len_m_lst = len(m_lst)
-max_len = max(len_n_lst, len_m_lst)
-min_len = min(len_n_lst, len_m_lst)
-
-for _ in range(min_len, max_len):
-    if len_n_lst > len_m_lst:
-        m_lst.append(m_lst[-1])
-    else:
-        n_lst.append(n_lst[-1])
+max_len = max(len(n_lst), len(m_lst))
+n_lst += [n_lst[-1]] * (max_len - len(n_lst))
+m_lst += [m_lst[-1]] * (max_len - len(m_lst))
 
 dev_lst = [n_lst[i] - m_lst[i] for i in range(max_len)]
 
 ans = 0
 last_sign = 0
     
-for x in dev_lst:
-    if x == 0:
-        continue
-    
-    curr_sign = 1 if x > 0 else -1
-
-    if last_sign != 0 and curr_sign != last_sign:
+for i in range(1, max_len):
+    if n_lst[i] == m_lst[i] and n_lst[i-1] != m_lst[i-1]:
         ans += 1
-    last_sign = curr_sign
 
 print(ans)
