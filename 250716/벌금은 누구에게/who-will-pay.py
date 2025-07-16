34c40231bfb97932c4c47c9c12424e5c7d79b4ac
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-16 13:47:12 +0900

    Title: 벌금은 누구에게, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250716/벌금은 누구에게/who-will-pay.py
+++ 250716/벌금은 누구에게/who-will-pay.py
@@ -1,9 +1,13 @@
 N, M, K = map(int, input().split())
 student = [0 for _ in range(N+1)]
 
+ans = -1
+
 for _ in range(M):
     penalty = int(input())
     student[penalty] += 1
     if student[penalty] >= K:
-        print(penalty)
+        ans = penalty
         break
+
+print(ans)
