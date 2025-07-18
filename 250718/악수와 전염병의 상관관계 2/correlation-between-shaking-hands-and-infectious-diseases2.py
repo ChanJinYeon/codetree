c25920dd48c3324fa9735d1b25bb469d4a6cce7e
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-18 15:30:22 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250718/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250718/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,10 +1,3 @@
-def infection(a: int, b: int, K: int) -> None:
-    global infected, cnt
-    if infected[a] and cnt[a] < K:
-        if not infected[b]:
-            infected[b] = True
-        cnt[a] += 1
-
 N, K, P, T = map(int, input().split())  # N명, K 전염병 수명, P 개발자, T번 악수
 handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 handshakes.sort(key=lambda T: T[0])
@@ -14,8 +7,12 @@ cnt = [0] * (N+1)
 infected[P] = True
 
 for _, a, b in handshakes:
-    infection(a, b, K)
-    infection(b, a, K)
+    ia, ib = infected[a], infected[b]
+    if ia and cnt[a] < K:
+        infected[b] = True
+        cnt[a] += 1
+    elif ib and cnt[b] < K:
+        infected[a] = True
+        cnt[b] += 1
 
-for i in range(1, len(infected)):
-    print(1, end='') if infected[i] else print(0, end='')
+print(''.join('1' if infected[i] else '0' for i in range(1, N+1)))
