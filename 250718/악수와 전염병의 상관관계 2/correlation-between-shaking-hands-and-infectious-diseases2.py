852d3fb490ee75caf21f2055846111051f7069c2
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-18 15:09:32 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250718/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250718/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,21 +1,21 @@
 def infection(a: int, b: int, K: int) -> None:
-    global dev
-    if (dev[a] == 0 or dev[b] == 0) and 0 < dev[a] + dev[b] <= K:
-        dev[a] += 1
-        dev[b] += 1
-    for i in range(1, len(dev)):
-        dev[i] += 1 if dev[i] != 0 and not (i == a or i == b) else 0
+    global infected, cnt
+    if infected[a] and cnt[a] < K:
+        if not infected[b]:
+            infected[b] = True
+        cnt[a] += 1
 
 N, K, P, T = map(int, input().split())  # N명, K 전염병 수명, P 개발자, T번 악수
 handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 handshakes.sort(key=lambda T: T[0])
 
-dev = [0 for _ in range(N+1)] # 값이 K가 되면 전염 X
-dev[P] = 1
+infected = [False] * (N+1)
+cnt = [0] * (N+1)
+infected[P] = True
 
-for p in handshakes:
-    a, b = p[1], p[2]
+for _, a, b in handshakes:
     infection(a, b, K)
+    infection(b, a, K)
 
-for i in range(1, len(dev)):
-    print(1, end='') if dev[i] > 0 else print(0, end='')
+for i in range(1, len(infected)):
+    print(1, end='') if infected[i] else print(0, end='')
