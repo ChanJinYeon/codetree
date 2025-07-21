774a5f3a0ec96a66bcc3130721ede99f5d81ac67
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-22 00:56:43 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250722/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 250722/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -13,16 +13,6 @@ drs, dcs = [1, 0, -1, 0], [0, 1, 0, -1] # S, E, N, W
 grid[0][0] = 1
 for _ in range(t-1):
     cnt += 1
-drs, dcs = [1, 0, -1, 0], [0, 1, 0, -1] # S, E, N, W
-
-grid[0][0] = 1
-for _ in range(t-1):
-    cnt += 1
-    nr, nc = r + drs[d], c + dcs[d]
-    if not in_range(nr, nc) or grid[nr][nc] != 0:
-        d = (d + 1) % 4
-
-    r, c = r + drs[d], c + dcs[d]
     nr, nc = r + drs[d], c + dcs[d]
     if not in_range(nr, nc) or grid[nr][nc] != 0:
         d = (d + 1) % 4
