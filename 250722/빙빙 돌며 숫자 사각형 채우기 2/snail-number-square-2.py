7e729b629f29272b5db4c0e157c03f2b1b47d471
이찬우 <105221020+ChanJinYeon@users.noreply.github.com>
2025-07-22 00:55:14 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 82ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250722/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,37 @@
+n, m = map(int, input().split())
+grid = [[0] * m for _ in range(n)]
+t = n * m
+
+def in_range(r: int, c: int) -> bool:
+    return 0 <= r < n and 0 <= c < m
+
+r = c = 0
+d = 0
+cnt = 1
+drs, dcs = [1, 0, -1, 0], [0, 1, 0, -1] # S, E, N, W
+
+grid[0][0] = 1
+for _ in range(t-1):
+    cnt += 1
+drs, dcs = [1, 0, -1, 0], [0, 1, 0, -1] # S, E, N, W
+
+grid[0][0] = 1
+for _ in range(t-1):
+    cnt += 1
+    nr, nc = r + drs[d], c + dcs[d]
+    if not in_range(nr, nc) or grid[nr][nc] != 0:
+        d = (d + 1) % 4
+
+    r, c = r + drs[d], c + dcs[d]
+
+
+$0
+    nr, nc = r + drs[d], c + dcs[d]
+    if not in_range(nr, nc) or grid[nr][nc] != 0:
+        d = (d + 1) % 4
+
+    r, c = r + drs[d], c + dcs[d]
+    grid[r][c] = cnt
+
+for row in grid:
+    print(*row, sep=' ')
